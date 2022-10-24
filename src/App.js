1220c55aefdf6ab5ecaa1d0d4388026e563208c1
Matias <matiasba_fb@hotmail.com>
2022-10-24 20:49:20 -0300

    hola

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
+import lupa from "./imagenes/lupa.svg"
 
 const PaisSeleccionado = (props) => {
   const [clima, setClima] = useState('')
@@ -15,21 +16,23 @@ const PaisSeleccionado = (props) => {
   }
   useEffect(hooks, [])
   return(
-    <>
-      <h2>{props.nombre}</h2>
-      <p>capital: {props.capital}</p>
-      <p>Poblacion: {props.poblacion}</p>
-      <h3>Idiomas</h3>
-      <ul>
-        {Object.values(props.idiomas).map((a,index)=>{
-          return <li key={index}>{a}</li>
-        })}
-      </ul>
-      <img src={props.bandera}></img>
-      <h3>Clima actual en {props.capital} {convertidor} °C</h3>
-      <p>Temperatura maxima: {((datos.temp_max)-273.5).toFixed(1)} °C</p>
-      <p>Temperatura minima: {((datos.temp_min)-273.5).toFixed(1)} °C</p>
-    </>
+    <div id='infoPais'>
+      <div id="contenidoInfoPais">
+        <img id="foto" src={props.bandera}></img>
+        <h2>{props.nombre}</h2>
+        <h4>Capital: {props.capital}</h4>
+        <h4>Poblacion: {props.poblacion}</h4>
+        <h4>Idiomas</h4>
+        <ul>
+          {Object.values(props.idiomas).map((a,index)=>{
+            return <li key={index}>{a}</li>
+          })}
+        </ul>
+        <h3>Temperatura actual en {props.capital} {convertidor} °C</h3>
+        <p>Temperatura maxima para hoy: {((datos.temp_max)-273.5).toFixed(1)} °C</p>
+        <p>Temperatura minima para hoy: {((datos.temp_min)-273.5).toFixed(1)} °C</p>
+      </div>
+    </div>
   )
 }
 
@@ -41,41 +44,52 @@ const Buscador = ({lista}) => {
   let filtrado = lista.filter((pais) => pais.name.official.toLowerCase().includes(buscado.toLocaleLowerCase()));
   if(buscado === ''){
     return(
-      <>
-        <input value={buscado} onChange={cambiarBusqueda}/>
-      </>
+      <div id='contenedorInicial'>
+        <h1>Busca algún pais</h1>
+        <div id='contenedorInicialInput'>
+          <input value={buscado} onChange={cambiarBusqueda} placeholder="Busca un pais..."/>
+        </div>
+      </div>
     )
   }else if(filtrado.length === 1){
     return(
-      <>
-        <input value={buscado} onChange={cambiarBusqueda}/>
+      <div id="resultadoEncontrado">
+        <div id='contenedorInputCoindencia'>
+          <input id="inputcoincidencia" value={buscado} onChange={cambiarBusqueda}/>
+        </div>
         <PaisSeleccionado nombre = {filtrado[0].name.common} capital={filtrado[0].capital} bandera={filtrado[0].flags.png} poblacion={filtrado[0].population} idiomas={filtrado[0].languages}/>
-      </>
+      </div>
     )
   }else if(buscado !== "" && filtrado.length === 0){
     return(
-      <>
-        <input value={buscado} onChange={cambiarBusqueda}/>
+      <div className='demasiadascoincidencias'>
+        <div className='buscadorDemasiadasCoincidencias'>
+          <input value={buscado} onChange={cambiarBusqueda}/>
+        </div>
         <p>No hay Resultados</p>
-      </>
+      </div>
     )
   }else if(filtrado.length > 0 && filtrado.length < 10){
     return(
-      <>
-        <input value={buscado} onChange={cambiarBusqueda}/>
+      <div id='listaResultados'>
+        <div id='contenedorInputListaResultados'>
+          <input value={buscado} onChange={cambiarBusqueda}/>
+        </div>
         <ul>
           {filtrado.map((pais) => 
-            <li key={pais.name.official}>{pais.name.official} <button onClick={()=>setBuscado(pais.name.official)}>x</button></li>)
+            <li key={pais.name.official}>{pais.name.official} <button onClick={()=>setBuscado(pais.name.official)}>Ver más</button></li>)
           }
         </ul>
-      </>
+      </div>
     )
   }else{
     return(
-      <>
-        <input value={buscado} onChange={cambiarBusqueda}/>
+      <div className='demasiadascoincidencias'>
+        <div className='buscadorDemasiadasCoincidencias'>
+          <input value={buscado} onChange={cambiarBusqueda}/>
+        </div>
         <p>Demasiadas coincidencias</p>
-      </>
+      </div>
     )
   }
 }
